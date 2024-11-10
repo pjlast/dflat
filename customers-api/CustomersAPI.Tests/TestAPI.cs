346fc6731aadbba58ba343a1dd48f6e0852c4dae
Petri-Johan Last <petri.last@sourcegraph.com>
2024-11-10 09:53:13 +0200

    feat: add GET /customers route

--- customers-api/CustomersAPI.Tests/TestAPI.cs
+++ customers-api/CustomersAPI.Tests/TestAPI.cs
@@ -24,5 +24,27 @@ public class TestAPI
         Assert.Equal(1, content?.Id);
         Assert.Equal("John", content?.FirstName);
         Assert.Equal("Jacobs", content?.LastName);
+
+        // Create Customer 2
+        payload = new CreateCustomerBody("Betty", "Burkes");
+        result = await client
+            .PostAsJsonAsync("/api/v1/customers", payload)
+            .ConfigureAwait(true);
+        Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+        content = await result.Content.ReadFromJsonAsync<Customer>().ConfigureAwait(true);
+        Assert.Equal(2, content?.Id);
+        Assert.Equal("Betty", content?.FirstName);
+        Assert.Equal("Burkes", content?.LastName);
+
+        // Fetch all customers
+#pragma warning disable CA2234
+        result = await client.GetAsync("/api/v1/customers").ConfigureAwait(true);
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        var customerList = await result.Content.ReadFromJsonAsync<List<Customer>>().ConfigureAwait(true);
+
+        Assert.NotNull(customerList);
+        Assert.Equal(2, customerList.Count);
     }
 }
--- customers-api/CustomersAPI/APIV1.cs
+++ customers-api/CustomersAPI/APIV1.cs
@@ -11,6 +11,11 @@ public static class CustomersAPIV1
         return TypedResults.Created($"/api/v1/customers/{customer.Id}", customer);
     }
 
+    static Ok<ICollection<Customer>> GetCustomers(IStore store)
+    {
+        return TypedResults.Ok(store.GetAll());
+    }
+
     public static RouteGroupBuilder MapCustomersAPI(this RouteGroupBuilder group)
     {
         group
@@ -18,11 +23,20 @@ public static class CustomersAPIV1
             .WithName("CreateCustomer")
             .WithOpenApi(generatedOperation =>
             {
-                generatedOperation.Summary = "Create a new customer";
+                generatedOperation.Summary = "Create a new customer.";
                 generatedOperation.Description =
                     "Create a new customer with the provided first and last names.";
                 return generatedOperation;
             });
+
+        group.MapGet("/", GetCustomers)
+            .WithName("GetAllCustomers")
+            .WithOpenApi(generatedOperation =>
+                    {
+                        generatedOperation.Summary = "Get all customers.";
+                        generatedOperation.Description = "Get all customers on the system.";
+                        return generatedOperation;
+                    });
         return group;
     }
 }
