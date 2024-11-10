df1de00db54c6c92ae845a5d8fce365ac500de45
Petri-Johan Last <petri.last@sourcegraph.com>
2024-11-10 07:23:40 +0200

    feat: add GET /transactions?accountId={id}

--- transactions-api/TransactionsAPI.Tests/TestAPI.cs
+++ transactions-api/TransactionsAPI.Tests/TestAPI.cs
@@ -25,5 +25,37 @@ public class TestAPI
         Assert.Equal(1, content?.Id);
         Assert.Equal(1, content?.AccountId);
         Assert.Equal(10, content?.Amount);
+
+        // Create a transaction for Account 2
+        payload = new CreateTransactionBody { accountId = 2, amount = 20 };
+        result = await client.PostAsJsonAsync("/api/v1/transactions", payload).ConfigureAwait(true);
+        Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+        content = await result.Content.ReadFromJsonAsync<Transaction>().ConfigureAwait(true);
+
+        Assert.Equal(2, content?.Id);
+        Assert.Equal(2, content?.AccountId);
+        Assert.Equal(20, content?.Amount);
+
+        // Fetch transactions for Account 2
+#pragma warning disable CA2234
+        result = await client.GetAsync("/api/v1/transactions?accountId=2").ConfigureAwait(true);
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        var transactionList = await result
+            .Content.ReadFromJsonAsync<List<Transaction>>()
+            .ConfigureAwait(true);
+
+        Assert.NotNull(transactionList);
+        Assert.Single(transactionList);
+        Assert.Equal(
+            new Transaction
+            {
+                Id = 2,
+                AccountId = 2,
+                Amount = 20,
+            },
+            transactionList[0]
+        );
     }
 }
--- transactions-api/TransactionsAPI/APIV1.cs
+++ transactions-api/TransactionsAPI/APIV1.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Transactions.Store;
 
 namespace Transactions.APIV1;
@@ -9,6 +10,15 @@ public static class TransactionsAPIV1
     {
         var transaction = store.Create(body.accountId, body.amount);
         return TypedResults.Created($"/api/v1/transactions/{transaction.Id}", transaction);
+    }
+
+    static Ok<ICollection<Transaction>> GetTransactionsByAccountId(
+        [FromQuery(Name = "accountId")] int accountId,
+        IStore store
+    )
+    {
+        var transactions = store.GetByAccountId(accountId);
+        return TypedResults.Ok(transactions);
     }
 
     public static RouteGroupBuilder MapTransactionsAPI(this RouteGroupBuilder group)
@@ -22,6 +32,17 @@ public static class TransactionsAPIV1
                 generatedOperation.Description =
                     "Create a new transaction for an account with the provided amount.";
 
+                return generatedOperation;
+            });
+
+        group
+            .MapGet("/", GetTransactionsByAccountId)
+            .WithName("GetTransactionsByAccountId")
+            .WithOpenApi(generatedOperation =>
+            {
+                generatedOperation.Summary = "Fetch all transactions belonging to an account.";
+                generatedOperation.Description =
+                    "Fetch all transactions that belong to the account with the provided ID.";
                 return generatedOperation;
             });
 
